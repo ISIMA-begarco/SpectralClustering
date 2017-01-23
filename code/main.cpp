c67c0ac203345399224d5050b4beba16ef83cdb5
begarco <benoit.garcon@outlook.com>
2017-01-23 15:28:33 +0100

    avancement partie methode

--- code/main.cpp
+++ code/main.cpp
@@ -3,31 +3,18 @@
 
 #include "CImg.h"
 #include "SpectralClustering.h"
-#include "KMeans.h"
 
 using namespace cimg_library;
 using namespace std;
 
-/*******************************************************************************
-
-                  Calcul de la fonction d'extraction d'attributs
-
-*******************************************************************************/
-
-void printmat(const CImg<float> & mat) {
-    cimg_forXY(mat,x,y)
-        cout << mat(x,y) << "\t";
-}
-
 int main(int argc, const char* argv []) {
-    string file = argc > 1 ? argv[1] : "small.bmp";
+    string file = argc > 1 ? argv[1] : "convexe.bmp";
     CImg<float> img = CImg<>(file.c_str()).channel(0);
 
     int ncl = argc > 2 ? atoi(argv[2]) : 2;    // Nombre de classes
-    int sig = argc > 3 ? atof(argv[3]) : 25.0f;    // Nombre de classes
+    int sig = argc > 3 ? atof(argv[3]) : 25.0f;    // Sigma
 
     SpectralClustering algo(sig);
-    KMeans km;
 
     CImg<float> etiquette = algo(img,ncl);
     etiquette.normalize(0,255);
